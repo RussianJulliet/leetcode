db6a69b9befb798355169fe0b4693236148f426a
Koniushenko Iulia <45033005+RussianJulliet@users.noreply.github.com>
2020-03-24 12:53:36 +0300

    O(n^2) brute force

--- container.py
+++ container.py
@@ -1,8 +1,5 @@
 list = list(map(int, input().split()))
-dict = {}
 max_water = 0
-for x in list:
-    dict[x] = list.index(x)
 for i in range(len(list)):
     for j in range(i + 1, len(list)):
         water = min(list[i], list[j]) * (j - i)
